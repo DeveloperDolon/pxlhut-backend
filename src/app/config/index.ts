07fb21c2cd461a6382ae04eb10fb1be484f42e7e
DeveloperDolon <dolonr718@gmail.com>
2025-04-16 15:34:33 +0600

    user model added with data set

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({path: path.join((process.cwd(), '.env'))});
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    database_url: process.env.MONGODB_URI,
-    port: process.env.PORT || 3000,
-}
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  database_url: process.env.MONGODB_URI,
+  port: process.env.PORT || 3000,
+  bcrypt_solid_rounds: process.env.BCRYPT_SALT_ROUNDS,
+};
---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,72 @@
+import { Schema } from 'mongoose';
+import { TUser, UserModel } from './user.interface';
+import { UserStatus } from './user.constant';
+import bcrypt from 'bcrypt';
+import config from '../../config';
+
+const userSchema = new Schema<TUser, UserModel>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user', 'superuser'],
+    },
+    status: {
+      type: String,
+      enum: UserStatus,
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_solid_rounds),
+  );
+
+  next();
+});
+
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+userSchema.statics.isUserExistByCustomId = async function (id: string) {
+  return await this.findOne({ id }).select('+password');
+};
